498b3a2a3a0aa83d2c9706ed388d84b60801f44d
David Gomez <dg.dave@hey.com>
2021-03-07 09:53:44 -0500

    Initial Commit

---
+++ Manyverse/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Manyverse
+//
+//  Created by David Gomez on 3/7/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Manyverse/ManyverseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ManyverseApp.swift
+//  Manyverse
+//
+//  Created by David Gomez on 3/7/21.
+//
+
+import SwiftUI
+
+@main
+struct ManyverseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
