c8362feed1f043d9a1cfbaab3c7838d50e4621d2
David Gomez <dg.dave@hey.com>
2021-03-08 18:16:23 -0500

    Setup nodejs-mobile

--- Manyverse/ManyverseApp.swift
+++ Manyverse/ManyverseApp.swift
@@ -9,9 +9,35 @@ import SwiftUI
 
 @main
 struct ManyverseApp: App {
+    let helper = AppHelper()
+    
+    init() {
+        if ProcessInfo.processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"] != "1" {
+            let nodejsThread = Thread(target:helper, selector:#selector(helper.startNode), object:nil)
+            nodejsThread.stackSize = 4*1024*1024;
+            nodejsThread.start()
+        }
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+
+class AppHelper {
+    @objc func startNode() {
+        var targetEnvironment = "iphoneos"
+        #if targetEnvironment(simulator)
+            targetEnvironment = "iphonesimulator"
+        #endif
+        let jsFile = Bundle.main.path(forResource: "backend/out/index.js", ofType: nil)!
+
+        let documentsPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
+        let bundlePath = Bundle.main.bundlePath
+//        Utils.debug("documentsPath \(documentsPath)")
+        NodeRunner.startEngine(withArguments: ["node", jsFile, documentsPath, bundlePath, targetEnvironment])
     }
 }
---
+++ Manyverse/NodeRunner.h
@@ -0,0 +1,16 @@
+//
+//  NodeRunner.h
+//  Manyverse
+//
+//  Created by David Gomez on 3/7/21.
+//
+
+#ifndef NodeRunner_h
+#define NodeRunner_h
+#import <Foundation/Foundation.h>
+
+@interface NodeRunner : NSObject {}
++ (void) startEngineWithArguments:(NSArray*)arguments;
+@end
+
+#endif /* NodeRunner_h */
