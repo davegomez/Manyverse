f179337e6d734e2ce3ae682c9e4bb250f5fc163a
David Gomez <dg.dave@hey.com>
2021-03-10 18:06:09 -0500

    Setup testing install and build phase with esbuild

--- backend/out/index.js
+++ backend/out/index.js
@@ -1,1 +1,8 @@
-console.log('Hello world!');
+(() => {
+  // package.json
+  var version = "0.0.0";
+
+  // src/index.ts
+  console.log("Hello from Node \u{1F44B}. Again!");
+  console.log("Version: ", version);
+})();
---
+++ backend/src/index.ts
@@ -0,0 +1,4 @@
+import { version } from "../package.json";
+
+console.log("Hello from Node 👋. Again!");
+console.log("Version: ", version);
